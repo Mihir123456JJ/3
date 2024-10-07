e95cf874ccb5a4270511ec541ce5e1d0cb746344
Mihir123456JJ <kalothiamihir@gmail.com>
2024-10-07 18:57:27 +0530

    Dictionary 3

---
+++ main.py
@@ -0,0 +1,6 @@
+code=  {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("Country code for INDIA",code.get("India","Not found"))
+print("Country code for CHINA",code.get("China","Not found"))
+print("Country code for AUSTRALIA",code.get("Australia","Not found"))
+print("Country code for NEPAL",code.get("Nepal","Not found"))
+ 
